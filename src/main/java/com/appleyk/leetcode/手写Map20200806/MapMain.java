48981becd0fb796b034179748a3ec29528abb439
yukun <yukun24@126.com>
2020-08-07 23:45:26 +0800

    手写代码实现HashMap功能完结版：（1）实现get （2）实现重复key的处理 （3）实现reSize+reHash,实际上就是数组扩容

--- src/main/java/com/appleyk/leetcode/手写Map20200806/MapMain.java
+++ src/main/java/com/appleyk/leetcode/手写Map20200806/MapMain.java
@@ -1,5 +1,8 @@
 package com.appleyk.leetcode.手写Map20200806;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * <p>越努力，越幸运</p>
  *
@@ -10,7 +13,7 @@ package com.appleyk.leetcode.手写Map20200806;
  */
 public class MapMain {
     public static void main(String[] args) {
-        MyMap<String,String> names = new TMap<>();
+        MyMap<String,String> names = new TMap<>(64);
         names.put("1","乔丹");
         names.put("2","科比");
         names.put("3","詹姆斯");
@@ -24,6 +27,30 @@ public class MapMain {
         names.put("11","安东尼");
         names.put("12","加内特");
         names.put("13","卡特");
-        System.out.println(names.size());
+        // 如果put的时候，不判断key值是否重复，而是直接新增一个entry的话，就失去了key值唯一的意义
+        names.put("14","加索尔");
+        names.put("15","洛瑞");
+        names.put("16","拜纳姆");
+        names.put("17","保罗");
+        names.put("18","罗斯");
+        names.put("19","罗伊");
+        names.put("20","布克");
+        names.put("21","哈登");
+        names.put("22","伊巴卡");
+        names.put("23","维斯布鲁克");
+        names.put("24","戈登");
+        names.put("25","霍华德");
+        names.put("26","马龙");
+        names.put("27","罗德曼");
+        names.put("28","吉诺比利");
+        names.put("29","诺维斯基");
+        names.put("30","帕克");
+        names.put("31","阿里纳斯");
+        // 随着key，value键值对entry对象越来越多时，如果map还是保持默认的16的话，getValue的效率就会降低
+        // 因为，hash碰撞会发生频繁，导致entry链表太长，遍历的时候，取key对应的val遍历的次数就会增加
+        // 怎么办呢，好办，扩容，对原有entry的key值进行重散列，重新定位在数组中的index
+        names.put("32","杜兰特");
+        System.out.println("Map Size :"+names.size());
+        System.out.println(names.get("23"));
     }
 }
--- src/main/java/com/appleyk/leetcode/手写Map20200806/TMap.java
+++ src/main/java/com/appleyk/leetcode/手写Map20200806/TMap.java
@@ -1,29 +1,53 @@
 package com.appleyk.leetcode.手写Map20200806;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * <p>越努力，越幸运</p>
+ * <p>
+ *     越努力，越幸运 实现Map接口，仿写JDK1.7版本的HashMap功能
+ *     1、非线程安全
+ *     2、利用hash+数组+链表来实现 key-value的存取
+ *     3、当存放的元素庞大时，会导致map的put和get效率会变低，两方面原因
+ *        3.1 没有指定capacity的大小，使用了默认的16，导致扩容（reSize+reHash）频繁，put效率降低
+ *        3.2 元素过多，在数组容量有限的情况下，key值进行hash计算得到的index值出现相同的概率增大，
+ *            导致index下标存放的链表长度过长，getValue的时候，增加了遍历的次数
+ * </p>
  *
  * @author appleyk
  * @version V.0.1.1
  * @blob https://blog.csdn.net/appleyk
  * @date created on 2020/5/19 10:46 PM
  */
-public class TMap<K,V> implements MyMap<K,V>{
+public class TMap<K, V> implements MyMap<K, V> {
 
-    /**加载因子*/
-    final float loadFactory = 0.75f;
-    /**默认数组长度*/
-    int defaultCapacity = 16;
-    /**节点数组*/
-    Node[] table;
-    int size ;
+    private static final int DEFAULT_CAPACITY = 1 << 4;
+    private static final float DEFAULT_LOAD_FACTORY = 0.75f;
 
-    TMap(){
-        table = new Node[defaultCapacity];
+    /**节点数组，Node实现了Entry接口，主要就是放key和value的*/
+    Node<K, V>[] table;
+
+    /**加载因子，和capacity值配合起来，可以决定table是否要扩容*/
+    private float loadFactory;
+
+    /**table数组容量(长度)*/
+    private int capacity;
+
+    /**Node元素个数*/
+    int size;
+
+    /**门面模式，虽然是两个构造器，其实指向的是一个*/
+    TMap() {
+        this(DEFAULT_CAPACITY, DEFAULT_LOAD_FACTORY);
     }
 
-    TMap(int capacity){
-        this.defaultCapacity = capacity;
+    TMap(int capacity) {
+        this(capacity, DEFAULT_LOAD_FACTORY);
+    }
+
+    TMap(int capacity, float loadFactory) {
+        this.capacity = capacity;
+        this.loadFactory = loadFactory;
         table = new Node[capacity];
     }
 
@@ -35,46 +59,123 @@ public class TMap<K,V> implements MyMap<K,V>{
     @Override
     public V put(K k, V v) {
 
+        // 再开始插入新node的时候，先判断下，是否有必要对table进行动态扩容
+        if (size >= capacity * loadFactory) {
+            // 扩容，默认扩容的临界值是元素的个数>=12
+            resize();
+        }
+
         // 如果key是null，永远放到第一位，HashMap是支持key是null的，Hashtable不支持
-        if(k == null){
-            Node<K,V> oldNode = table[0];
-            table[0] = new Node(null,v);
+        if (k == null) {
+            Node<K, V> oldNode = table[0];
+            table[0] = new Node(null, v);
             return oldNode.getValue();
         }
 
-        // 否者的话，得通过key值计算hash值，主要是为了找key在table数组中的下标索引
+        // 如果，得通过key值计算hash值，主要是为了找key在table数组中的下标索引
         int index = getIndex(k);
         // 先把原有该索引处的node保存一下
-        Node<K,V> oldNode = table[index];
-        // 然后将现有的node放上去
-        Node newNode = new Node(k,v);
+        Node<K, V> oldNode = table[index];
+        // 循环判断下，k值是不是存在了，如果存在了，就要更新对应entry的value
+        Node<K, V> node = oldNode;
+        while (node != null) {
+            if (k.equals(node.getKey())) {
+                node.setValue(v);
+                return node.getValue();
+            }
+            node = node.next;
+        }
+        // 然后将现有的node放进去
+        Node newNode = new Node(k, v);
         table[index] = newNode;
         /**设置下一个节点*/
         newNode.setNext(oldNode);
-        return oldNode!=null ? oldNode.getValue() : null;
+        size++;
+        return oldNode != null ? oldNode.getValue() : null;
     }
 
+    /***
+     * 1、重新生成一个newTable，长度为当前数组容量*2
+     * 2、重新计算key的hash值，将原有旧表中的entry节点打散放到新表中
+     */
+    private void resize() {
+        long start = System.currentTimeMillis();
+        System.out.println("=== 发生了一次扩容 ===");
+        // 默认扩容*2，就原来默认是16的话，扩容后数组的length就是32
+        this.capacity *=2;
+        // 记住，这个size别漏掉了，要重新归0，否则不仅会增加扩容的次数，还会导致最终的元素个数不对
+        size = 0;
+        reHash(new Node[capacity]);
+        long end = System.currentTimeMillis();
+        System.out.println("扩容耗时："+(end-start)+"ms");
+    }
 
-    /**通过key计算得出其所在的node在table中的哪一个下标处*/
-    private int getIndex(K k){
-        // 一个key的hashCode 与 数组长度-1的值进行位与运算，永远不会在[0,defaultCapacity-1]之间
-        return k.hashCode() & (defaultCapacity-1);
+    private void reHash(Node<K,V>[] newTable){
+        // 1、遍历原来的table中的entry，将所有的entry记录下来
+        List<Node<K,V>> allNodes = new ArrayList<>();
+        for (int i = 0; i < table.length ; i++) {
+            Node<K, V> node = table[i];
+            while( node != null){
+                allNodes.add(node);
+                node = node.next;
+            }
+        }
+
+        // 2、将新表赋给旧表，也就是table指向新的内存地址
+        this.table = newTable;
+
+        // 3、重新打散收集到的所有的entry，其实就是调用类中的put方法
+        for (Node<K, V> node : allNodes) {
+            put(node.getKey(),node.getValue());
+        }
+
+        // GC回收
+        newTable = null;
+    }
+
+    /**
+     * 通过key计算得出其所在的node在table中的哪一个下标处
+     */
+    private int getIndex(K k) {
+        // 一个key的hashCode 与 数组长度-1的值进行位与运算，永远会在[0,defaultCapacity-1]之间
+        return k.hashCode() & (capacity - 1);
     }
 
     @Override
     public V get(K k) {
+
+        // 取的时候，就就有讲究了，你要先判断key是不是null，null的话，直接返回第一个元素
+        if (k == null) {
+            return table[0].getValue();
+        }
+
+        // 否则的话，和put一样，先进行hash值，算出index的值
+        int index = getIndex(k);
+
+        // 确定了index的位置后，判断下，有没有值，也就是虽然hash算出了index，但那个位置不一样有entry啊
+        Node<K, V> node = table[index];
+        if (node == null) {
+            return null;
+        }
+        // 如果有的话，那就好办了，循环该位置上的链表，判断key是哪一个entry对象的，找到他，拿出他的value值即可
+        while (node != null) {
+            if (k.equals(node.getKey())) {
+                return node.getValue();
+            }
+            node = node.next;
+        }
         return null;
     }
 
-    class Node<K,V> implements MyMap.Entry<K,V>{
+    class Node<K, V> implements MyMap.Entry<K, V> {
 
         K k;
         V v;
-        Node<K,V> next;
+        Node<K, V> next;
 
-        Node(K k ,V v){
-            this.k = k ;
-            this.v = v ;
+        Node(K k, V v) {
+            this.k = k;
+            this.v = v;
         }
 
         @Override
@@ -95,7 +196,7 @@ public class TMap<K,V> implements MyMap<K,V>{
 
         @Override
         public Entry<K, V> next() {
-            if(next!=null){
+            if (next != null) {
                 return this.next;
             }
             return null;
