f149f2cc28dcc746ec5f3cf6737d931b328a4644
yukun <yukun24@126.com>
2020-09-04 00:10:49 +0800

    增加GC垃圾收集器组合参数的使用及GC日志打印

--- src/main/java/com/appleyk/gc/GcTest01.java
+++ src/main/java/com/appleyk/gc/GcTest01.java
@@ -4,20 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * <p>越努力，越幸运</p>
+ * <p>Full GC测试</p>
  *
  * @author appleyk
  * @version V.0.1.1
  * @blob https://blog.csdn.net/appleyk
  * @date created on 2020/5/19 10:46 PM
  */
-public class GcTest {
+public class GcTest01 {
     byte[] a = new byte[1024*100]; // 100kb
     public static void main(String[] args) {
         // names是线程栈的局部变量，它是gcroot
-        List<GcTest> names = new ArrayList<>();
+        List<GcTest01> names = new ArrayList<>();
         while (true) {
-            names.add(new GcTest());
+            names.add(new GcTest01());
             try {
                 Thread.sleep(10);
             } catch (InterruptedException e) {
