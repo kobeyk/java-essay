df720e2986e5bbc2a3efbfe39f28c6e5d2915378
yukun <yukun24@126.com>
2020-10-14 22:49:53 +0800

    增加jdk动态代理和cglib动态代理简单实用案例

---
+++ src/main/java/com/appleyk/proxy/jdk动态代理/HelloJDK1.java
@@ -0,0 +1,13 @@
+package com.appleyk.proxy.jdk动态代理;
+
+/**
+ * <p>越努力，越幸运</p>
+ *
+ * @author appleyk
+ * @version V.0.1.1
+ * @blob https://blog.csdn.net/appleyk
+ * @date created on  10:00 下午 2020/10/14
+ */
+public interface HelloJDK1 {
+    void say1();
+}
---
+++ src/main/java/com/appleyk/proxy/jdk动态代理/HelloJDKImpl2.java
@@ -0,0 +1,26 @@
+package com.appleyk.proxy.jdk动态代理;
+
+import java.lang.reflect.Proxy;
+
+/**
+ * <p>越努力，越幸运</p>
+ *
+ * @author appleyk
+ * @version V.0.1.1
+ * @blob https://blog.csdn.net/appleyk
+ * @date created on  10:13 下午 2020/10/14
+ */
+public class HelloJDKImpl2 implements HelloJDK1 {
+
+    @Override
+    public void say1() {
+        System.out.println("say1:hello world! -- from 2");
+    }
+
+    public static void main(String[] args) {
+        HelloJDK1 helloJDK2 = (HelloJDK1)Proxy.newProxyInstance(HelloJDKImpl2.class.getClassLoader(),
+                new Class[]{HelloJDK1.class},
+                new HelloJDKInvocationHandler(new HelloJDKImpl2()));
+        helloJDK2.say1();
+    }
+}
