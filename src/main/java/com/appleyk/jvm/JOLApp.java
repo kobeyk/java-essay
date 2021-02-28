742e494808b33b4c65c4f861c7aa550f0e457da6
appleyk <yukun24@126.com>
2021-02-28 10:50:14 +0800

    对JVM运行时数据区中的虚拟机栈（也叫线程栈）中的栈帧的局部变量表中的slot复用的概念进行代码演示，同时指出，long和double变量各占两个slot位

--- src/main/java/com/appleyk/jvm/JOLApp.java
+++ src/main/java/com/appleyk/jvm/JOLApp.java
@@ -11,12 +11,28 @@ import org.openjdk.jol.info.ClassLayout;
  * @date created on 11:19 2020/10/26
  */
 public class JOLApp {
+    private static int age = 10;
     public static void main(String[] args) {
         int a  = 6,b = 3;
         int c = a+b;
         System.out.println(c);
         Object obj = new Object();
         System.out.println(ClassLayout.parseInstance(obj).toPrintable());
+    }
 
+    /**
+     * 局部变量表每个存放变量的单元叫slot（插槽）
+     * 下面演示了slot的重复利用
+     */
+    public void A(){
+        int a =1;
+        {
+            int b;
+            b = a+2;
+        }
+        // double 和 long 各占两个slot
+        double salary = 10.0d;
+        long count = 10L;
+        int c = 3 + a;
     }
 }
